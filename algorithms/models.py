adce3c35a3d2dab452f7a011c05d52d70840f8af
unknown <Aulas Ingenieria@12C302E08210712.JAVERIANA.LOC>
2023-10-12 12:15:40 -0500

    added test

--- algorithms/models.py
+++ algorithms/models.py
@@ -10,16 +10,32 @@ from skimage import measure
 from strawberry.file_uploads import Upload
 
 
+@strawberry.input
+class BinarizationInput:
+    threshold: int
+    file: Upload
+
+
+@strawberry.input
+class MarchingInput:
+    file: Upload
+
+@strawberry.input
+class AverageInput:
+    file: Upload
+
+
+
 class Queries:
 
-    async def binarization(self, threshold: int, file: Upload) -> str:
-        image = await file.read()
+    async def binarization(self, inpt: BinarizationInput) -> str:
+        image = await inpt.file.read()
         dicom_dataset = pydicom.dcmread(io.BytesIO(image))
 
         # Convertir los píxeles DICOM a una matriz NumPy
         pixel_array = dicom_dataset.pixel_array
 
-        binarized_array = pixel_array > threshold
+        binarized_array = pixel_array > inpt.threshold
 
         binarized_image = Image.fromarray((binarized_array * 255).astype(np.uint8))
 
@@ -38,8 +54,8 @@ class Queries:
 
         return response
 
-    async def marching_squares(self, file: Upload) -> str:
-        image = await file.read()
+    async def marching_squares(self, inpt: MarchingInput) -> str:
+        image = await inpt.file.read()
         ds = pydicom.dcmread(io.BytesIO(image))
         image = ds.pixel_array
 
@@ -58,8 +74,8 @@ class Queries:
 
         return response
 
-    async def average_and_deviation(self, file: Upload) -> str:
-        image = await file.read()
+    async def average_and_deviation(self, inpt: AverageInput) -> str:
+        image = await inpt.file.read()
         ds = pydicom.dcmread(io.BytesIO(image))
         image = ds.pixel_array
 
@@ -79,6 +95,3 @@ class Queries:
         response = json.dumps(result)
 
         return response
-
-
-
--- main.py
+++ main.py
@@ -4,45 +4,27 @@ import py_eureka_client.eureka_client as eureka_client
 import strawberry
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from strawberry.asgi import GraphQL
 
 from algorithms.models import Queries
 
+app_name = 'models-python'
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "Hola, mundo!"
-
-
-@strawberry.type
-class Mutation:
     marching_squares: str = strawberry.field(resolver=Queries.marching_squares)
     binarization: str = strawberry.field(resolver=Queries.binarization)
     average_and_desviation: str = strawberry.field(resolver=Queries.average_and_deviation)
 
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+schema = strawberry.Schema(query=Query)
 
 graphql_app = GraphQL(schema)
 
 app = FastAPI()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
-
-origins = [
-    "http://localhost:4200",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 def get_server_info():
@@ -55,7 +37,7 @@ def get_server_info():
 async def main():
     ip = get_server_info()
     await eureka_client.init_async(eureka_server="http://eureka:8761",
-                                   app_name="example-model",
+                                   app_name=app_name,
                                    instance_port=8000,
                                    metadata={"microservice-type": "IA-MODEL"},
                                    instance_host=ip
